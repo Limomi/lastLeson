b7ce7ddc40a92745b1992af1580b0e91a1f335c2
Limoni <figulgago@gmail.com>
2025-02-03 23:32:32 +0500

    git init

---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Service;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Service::insert(
+            [
+                [
+                    'title' => 'общий клиниг',
+                ],
+                [
+                    'title' => 'Генеральная уборка',
+                ],
+                [
+                    'title' => 'Послестроительная уборка'
+                ],
+                [
+                    'title' => 'Химчстка ковров'
+                ]
+            ]
+            );
+    }
+}
